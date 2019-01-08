c382cebe645b6f3d666bd27534b052ba4ea6566b
Iain Diamond <iain.diamond@gmail.com>
2019-01-08 01:31:15 +0000

    Use named return value

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,10 +1,12 @@
 package accumulate
 
+// Operation transforms a string into another string
+type Operation = func(string) string
+
 // Accumulate performs provided operation on the given collection
-func Accumulate(collection []string, operation func(string) string) []string {
-	output := make([]string, 0)
+func Accumulate(collection []string, operation Operation) (output []string) {
 	for _, elem := range collection {
 		output = append(output, operation(elem))
 	}
-	return output
+	return
 }
