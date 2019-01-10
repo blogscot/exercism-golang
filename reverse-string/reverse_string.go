69d67efdc5d84038db97dc0ae33ac57b98b47a40
Iain Diamond <iain.diamond@gmail.com>
2019-01-10 12:54:56 +0000

    Improve performance using runes

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,9 +1,12 @@
 package reverse
 
 // String reverses a string
-func String(text string) (out string) {
-	for _, letter := range text {
-		out = string(letter) + out
+func String(text string) string {
+	runes := []rune(text)
+	length := len(runes)
+
+	for i := 0; i < length/2; i++ {
+		runes[length-1-i], runes[i] = runes[i], runes[length-1-i]
 	}
-	return
+	return string(runes)
 }
