bfac8426afa59472917dd65f34d715e4860c280b
Iain Diamond <iain.diamond@gmail.com>
2019-01-15 14:10:03 +0000

    one loop to rule them all

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,41 +1,36 @@
 package luhn
 
 import (
-	"strings"
 	"unicode"
 )
 
 // Valid determines if a number is a valid Luhn number
 func Valid(number string) bool {
-	filtered := strings.Map(func(r rune) rune {
-		if unicode.IsSpace(r) {
-			return -1
-		}
-		return r
-	}, number)
-
-	if len(filtered) < 2 || !allDigits(filtered) {
-		return false
-	}
-
 	total := 0
+	count := 0
 
-	for index, r := range filtered {
+	for index := len(number) - 1; index >= 0; index-- {
+		r := rune(number[index])
+		if r == ' ' {
+			continue
+		}
+		if !unicode.IsDigit(r) {
+			return false
+		}
 		digit := int(r - '0')
-		if (len(filtered)-1-index)&1 == 1 {
+		if count&1 == 1 {
 			digit = digit * 2
 			if digit > 9 {
 				digit -= 9
 			}
 		}
 		total += digit
+		count++
+	}
+
+	if count < 2 {
+		return false
 	}
 
 	return total%10 == 0
-}
-
-func allDigits(number string) bool {
-	return strings.IndexFunc(number, func(r rune) bool {
-		return !unicode.IsNumber(r)
-	}) == -1
 }
