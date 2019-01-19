6e525a80b76b74d62382c36dfd57eac6b5fb1d83
Iain Diamond <iain.diamond@gmail.com>
2019-01-19 13:17:06 +0000

    return named parameter

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -2,19 +2,19 @@ package letter
 
 // ConcurrentFrequency calculates the letter frequency of the given
 // texts concurrently.
-func ConcurrentFrequency(texts []string) FreqMap {
+func ConcurrentFrequency(texts []string) (freqMap FreqMap) {
 	freqChan := make(chan FreqMap)
 
 	for _, text := range texts {
 		go func(s string) { freqChan <- Frequency(s) }(text)
 	}
 
-	freqMap := FreqMap{}
+	freqMap = FreqMap{}
 
 	for range texts {
 		for k, v := range <-freqChan {
 			freqMap[k] += v
 		}
 	}
-	return freqMap
+	return
 }
