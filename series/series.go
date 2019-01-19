0244d205a5e0496bd9c06d47eebe3b591515e9da
Iain Diamond <iain.diamond@gmail.com>
2019-01-19 22:24:58 +0000

    Refactor solution

--- series/series.go
+++ series/series.go
@@ -2,11 +2,6 @@ package series
 
 // All returns a list of all substrings of s with length n.
 func All(n int, s string) (out []string) {
-	if len(s) < n {
-		return nil
-	}
-	out = []string{}
-
 	for index := 0; index <= len(s)-n; index++ {
 		out = append(out, s[index:index+n])
 	}
@@ -15,16 +10,13 @@ func All(n int, s string) (out []string) {
 
 // UnsafeFirst returns the first substring of s with length n.
 func UnsafeFirst(n int, s string) string {
-	if substrings := All(n, s); substrings != nil {
-		return substrings[0]
-	}
-	return ""
+	return s[0:n]
 }
 
 // First returns the first substring of s with length n.
 func First(n int, s string) (first string, ok bool) {
-	if substrings := All(n, s); substrings != nil {
-		return substrings[0], true
+	if n > len(s) {
+		return "", false
 	}
-	return "", false
+	return s[0:n], true
 }
